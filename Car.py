6cd283bc74e4284d2d89cbf990153d961976a2cc
Qingci-Wayne-Luo <119480103+Qingci-Wayne-Luo@users.noreply.github.com>
2023-02-02 17:07:44 -0500

    Add files via upload

---
+++ Car.py
@@ -0,0 +1,69 @@
+# Car.py
+
+class Car:
+    def __init__(self, make, model, year, price):
+        self.make = make.upper()
+        self.model = model.upper()
+        self.year = year
+        self.price = price
+
+    def __gt__(self, rhs):
+        if self.make > rhs.make:
+            return True
+        elif self.make == rhs.make:
+            if self.model > rhs.model:
+                return True
+            elif self.model == rhs.model:
+                if self.year > rhs.year:
+                    return True
+                elif self.year == rhs.year:
+                    if self.price > rhs.price:
+                        return True
+                    else:
+                        return False
+        return False
+
+    def __lt__(self, rhs):
+        if self.make < rhs.make:
+            return True
+        elif self.make == rhs.make:
+            if self.model < rhs.model:
+                return True
+            elif self.model == rhs.model:
+                if self.year < rhs.year:
+                    return True
+                elif self.year == rhs.year:
+                    if self.price < rhs.price:
+                        return True
+                    else:
+                        return False
+        return False
+
+
+    def __eq__(self, rhs):
+        if self.make == rhs.make:
+            if self.model == rhs.model:
+                if self.year == rhs.year:
+                    if self.price == self.price:
+                        return True
+                    else:
+                        False
+                else:
+                    False
+            else:
+                return False
+        else:
+            return False
+           
+    def __str__(self):
+        return "Make: {}, Model: {}, Year: {}, Price: ${}"\
+               .format(self.make, self.model, self.year, self.price)
+        
+
+
+
+
+
+    
+        
+        
